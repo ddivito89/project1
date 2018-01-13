adc930ab98053bf5f4e627c39b21204afe135627
Brian Parkes <parkesbrianj@gmail.com>
2018-01-13 08:55:19 -0600

    in process of writing html for creating question box

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,27 @@
 //Answer questions to narrow down search
-	//Header
-	//List of questions
-	//Submit button
+
+var restaurantCounter = 0;	//Counter for each set of html containers
+var questions = ['first', 'second', 'third', 'fourth'];	//store text for input fields
+
+function makeQuestionsContainer() {
+	//Create container for questions
+	var container = $('<div>').addClass('container questions').attr('restaurant-number', restaurantCounter);
+
+	//Create elements for questions and user inputs
+	var searchQuestions = $('<form>').addClass('form-group');
+	for (var i = 0; i < questions.length; i++) {
+		var input = $('<input>').attr('question-number', i).attr('type', 'text').attr('placeholder', questions[i]);
+		searchQuestions.append(input);
+	}
+
+	//Create submission element button
+	var submitButton = $('<input>').addClass('btn btn-primary').attr('type', 'submit');
+
+	//Put elements together
+	container.append(searchQuestions).append(submitButton);
+	$('body').append(container);
+}
+
 
 //Display results of restaurant search
 	//Show results side by side
