daddd857007738be1fea2bf658ba17e96518e2c6
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-01-13 10:42:44 -0600

    put api query into function buttons can reference

--- logic.js
+++ logic.js
@@ -1,49 +1,44 @@
-$("#search-keyword").on("click", function() {
-  event.preventDefault();
-
+function getData(index) {
   var apiKey = "2d0ccedc51dbe25f162cd3e019b158de";
   var keyword = $("#keyword-input").val();
   var lat = "41.884653";
   var lon = "-87.627404599";
-  var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + keyword +
-  "&lat="+lat+"&lon="+lon+"&sort=real_distance&order=desc%20Response%20Body" + "&apikey=" + apiKey;
+  var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + keyword + "&lat=" + lat + "&lon=" + lon +
+  "&sort=real_distance&order=desc%20Response%20Body" + "&apikey=" + apiKey;
 
   console.log(queryURL)
 
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).done(function(response) {
+  $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
     console.log(response)
-    for(var x=0; x<3; x++)
-    {
-      var name = response.restaurants[x].restaurant.name
-      var address = response.restaurants[x].restaurant.location.address
-      var cuisines = response.restaurants[x].restaurant.cuisines
-      var restLat = response.restaurants[x].restaurant.location.latitude
-      var restLon = response.restaurants[x].restaurant.location.longitude
 
-      var newOptions = $("<div class='col-md-4'></div>")
+    function showOptions(index) {
 
-      newOptions.append(`<p>${name}</p>`)
-      newOptions.append(`<p>${address}</p>`)
-      newOptions.append(`<p>${cuisines}</p>`)
-      newOptions.append(`<p>${restLat}</p>`)
-      newOptions.append(`<p>${restLon}</p>`)
-      newOptions.append(`<button>Select</button>`)
+      $("#test-div").empty()
+      for (var x = index; x < index + 3; x++) {
+        var name = response.restaurants[x].restaurant.name
+        var address = response.restaurants[x].restaurant.location.address
+        var cuisines = response.restaurants[x].restaurant.cuisines
+        var restLat = response.restaurants[x].restaurant.location.latitude
+        var restLon = response.restaurants[x].restaurant.location.longitude
 
+        var newOptions = $("<div class='col-md-4'></div>")
 
-      $("#test-div").append(newOptions)
+        newOptions.append(`<p>${name}</p>`)
+        newOptions.append(`<p>${address}</p>`)
+        newOptions.append(`<p>${cuisines}</p>`)
+        newOptions.append(`<p>${restLat}</p>`)
+        newOptions.append(`<p>${restLon}</p>`)
+        newOptions.append(`<button>Select</button>`)
 
-
+        $("#test-div").append(newOptions)
+      }
     }
-
-
-
-
-
+    showOptions(index)
   });
+}
 
-
-
+$("#search-keyword").on("click", function() {
+  var index = 0;
+  event.preventDefault();
+  getData(index)
 })
