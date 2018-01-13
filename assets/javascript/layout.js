c2bb4cd9afe81923ea1876ae0ffc63e19c168b32
Brian Parkes <parkesbrianj@gmail.com>
2018-01-13 13:03:31 -0600

    hard coded input fields into HTML as opposed to javascript construction

--- assets/javascript/layout.js
+++ assets/javascript/layout.js
@@ -1,33 +1,33 @@
 //Answer questions to narrow down search
 
-var restaurantCounter = 0;	//Counter for each set of html containers
-var questions = ['cuisine', 'rating', 'cost'];	//store text for input fields
+// var restaurantCounter = 0;	//Counter for each set of html containers
+// var questions = ['cuisine', 'rating', 'cost'];	//store text for input fields
 
-function makeQuestionsContainer() {
-	//Create container for questions
-	var container = $('<div>').addClass('container questions').attr('restaurant-number', restaurantCounter);
-	//row to contain header
-	container.append($('<header>').addClass('row'));
-	//Row to contain input fields
-	container.append($('<div>'));
+// function makeQuestionsContainer() {
+// 	//Create container for questions
+// 	var container = $('<div>').addClass('container questions').attr('restaurant-number', restaurantCounter);
+// 	//row to contain header
+// 	container.append($('<header>').addClass('row'));
+// 	//Row to contain input fields
+// 	container.append($('<div>'));
 
-	//Create elements for questions and user inputs
-	var searchQuestions = $('<form>').addClass('form-horizontal');
-	for (var i = 0; i < questions.length; i++) {
-		var input = $('<div>').addClass('form-group').append($('<input>').attr('id', questions[i]).attr('type', 'text').attr('placeholder', questions[i]));
-		searchQuestions.append(input);
-	}
+// 	//Create elements for questions and user inputs
+// 	var searchQuestions = $('<form>').addClass('form-horizontal');
+// 	for (var i = 0; i < questions.length; i++) {
+// 		var input = $('<div>').addClass('form-group').append($('<input>').attr('id', questions[i]).attr('type', 'text').attr('placeholder', questions[i]));
+// 		searchQuestions.append(input);
+// 	}
 
-	//Create submission element button
-	var submitButton = $('<div>').addClass('form-group').append($('<input>').addClass('btn btn-primary').attr('type', 'submit').attr('id', 'submit-keys'));
+// 	//Create submission element button
+// 	var submitButton = $('<div>').addClass('form-group').append($('<input>').addClass('btn btn-primary').attr('type', 'submit').attr('id', 'submit-keys'));
 
-	//Put elements together
-	container.children('div').append(searchQuestions);
-	container.children('div').children('form').append(submitButton);
-	$('body').append(container);
-	$('body').append('<div id="test-div" class="row"></div>');
-}
-makeQuestionsContainer()
+// 	//Put elements together
+// 	container.children('div').append(searchQuestions);
+// 	container.children('div').children('form').append(submitButton);
+// 	$('body').append(container);
+// 	$('body').append('<div id="test-div" class="row"></div>');
+// }
+// makeQuestionsContainer()
 
 //Display results of restaurant search
 	//Show results side by side
