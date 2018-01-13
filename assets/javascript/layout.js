c2d830230df806764f74dd88bd6d1bfa3dcb5e1a
Brian Parkes <parkesbrianj@gmail.com>
2018-01-13 13:47:20 -0600

    working on choice container javascript

--- assets/javascript/layout.js
+++ assets/javascript/layout.js
@@ -1,43 +1,56 @@
-//Answer questions to narrow down search
+var restaurantCounter = 0;	//Counter for each set of html containers
 
-// var restaurantCounter = 0;	//Counter for each set of html containers
-// var questions = ['cuisine', 'rating', 'cost'];	//store text for input fields
+//Restaurant Choice Container Builder
+function makeChosenContainer() {
+	var container = $('<div>').addClass('container').attr('choice-number', restaurantCounter);
+	container.append($('<div>').addClass('row'));
+	container.children('div')
+		.append($('<div>').addClass('col-md-8 info'))
+		.append($('<div>').addClass('col-md-4 map'));
 
-// function makeQuestionsContainer() {
-// 	//Create container for questions
-// 	var container = $('<div>').addClass('container questions').attr('restaurant-number', restaurantCounter);
-// 	//row to contain header
-// 	container.append($('<header>').addClass('row'));
-// 	//Row to contain input fields
-// 	container.append($('<div>'));
+	$('body').append(container);
+}
 
-// 	//Create elements for questions and user inputs
-// 	var searchQuestions = $('<form>').addClass('form-horizontal');
-// 	for (var i = 0; i < questions.length; i++) {
-// 		var input = $('<div>').addClass('form-group').append($('<input>').attr('id', questions[i]).attr('type', 'text').attr('placeholder', questions[i]));
-// 		searchQuestions.append(input);
-// 	}
-
-// 	//Create submission element button
-// 	var submitButton = $('<div>').addClass('form-group').append($('<input>').addClass('btn btn-primary').attr('type', 'submit').attr('id', 'submit-keys'));
-
-// 	//Put elements together
-// 	container.children('div').append(searchQuestions);
-// 	container.children('div').children('form').append(submitButton);
-// 	$('body').append(container);
-// 	$('body').append('<div id="test-div" class="row"></div>');
-// }
-// makeQuestionsContainer()
-
-//Display results of restaurant search
-	//Show results side by side
-	//Button to select restaurant
+makeChosenContainer();
 
 //Display details of the chosen restaurant
-	//picture
-	//Map
-	//Link to menu
-
 //Enter review info of restaurant
 	//Rating 1-10
 	//Text box to enter comments
+
+
+
+
+
+
+
+
+
+
+
+
+//Don't need this code, but keeping it for copy paste
+	// function makeQuestionsContainer() {
+	// 	//Create container for questions
+	// 	var container = $('<div>').addClass('container questions').attr('restaurant-number', restaurantCounter);
+	// 	//row to contain header
+	// 	container.append($('<header>').addClass('row'));
+	// 	//Row to contain input fields
+	// 	container.append($('<div>'));
+
+	// 	//Create elements for questions and user inputs
+	// 	var searchQuestions = $('<form>').addClass('form-horizontal');
+	// 	for (var i = 0; i < questions.length; i++) {
+	// 		var input = $('<div>').addClass('form-group').append($('<input>').attr('id', questions[i]).attr('type', 'text').attr('placeholder', questions[i]));
+	// 		searchQuestions.append(input);
+	// 	}
+
+	// 	//Create submission element button
+	// 	var submitButton = $('<div>').addClass('form-group').append($('<input>').addClass('btn btn-primary').attr('type', 'submit').attr('id', 'submit-keys'));
+
+	// 	//Put elements together
+	// 	container.children('div').append(searchQuestions);
+	// 	container.children('div').children('form').append(submitButton);
+	// 	$('body').append(container);
+	// 	$('body').append('<div id="test-div" class="row"></div>');
+	// }
