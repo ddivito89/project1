55c886f6ea98487853bb0987d2406427958eb497
Brian Parkes <parkesbrianj@gmail.com>
2018-01-13 08:17:18 -0600

    Basic structure added in html for reference

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,17 @@
+//Answer questions to narrow down search
+	//Header
+	//List of questions
+	//Submit button
+
+//Display results of restaurant search
+	//Show results side by side
+	//Button to select restaurant
+
+//Display details of the chosen restaurant
+	//picture
+	//Map
+	//Link to menu
+
+//Enter review info of restaurant
+	//Rating 1-10
+	//Text box to enter comments
